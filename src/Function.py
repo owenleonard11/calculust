c21faf30edf0c6cebefddc37742822c143cde5f4
Owen Leonard <owenleonard11@gmail.com>
2022-04-13 16:37:52 -0400

    Move Constant and Variable to defaults.py

--- src/Function.py
+++ src/Function.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Any
 from abc import ABCMeta, abstractmethod
 
-__all__ = ['FunctionMeta', 'FunctionABCMeta', 'Function', 'Constant', 'Variable']
+__all__ = ['FunctionMeta', 'FunctionABCMeta', 'Function']
 
 class FunctionMeta(type):
     """A metaclass whose instances are function classes"""
@@ -54,21 +54,3 @@ class Function(metaclass=FunctionABCMeta):
     
     def simplify(self) -> Function:
         return self
-
-class Constant(Function):
-    value: Any
-
-    def eval(self, **kwargs) -> Function:
-        return Constant(self.value)
-    
-    def __str__(self) -> str:
-        return str(self.value)
-        
-class Variable(Function):
-    name: str
-
-    def eval(self, **kwargs) -> Function:
-        return kwargs[self.name] if self.name in kwargs else Variable(self.name)
-
-    def __str__(self) -> str:
-        return self.name
--- src/defaults.py
+++ src/defaults.py
@@ -1,5 +1,34 @@
 import operator as op
+from typing import Any
+
 from definition_utils import *
+from Function import Function
 
-# arithmetic operations
+# constants and variables
+class Constant(Function):
+    value: Any
 
+    def eval(self, **kwargs) -> Function:
+        return Constant(self.value)
+    
+    def __str__(self) -> str:
+        return str(self.value)
+        
+class Variable(Function):
+    name: str
+
+    def eval(self, **kwargs) -> Function:
+        return kwargs[self.name] if self.name in kwargs else Variable(self.name)
+
+    def __str__(self) -> str:
+        return self.name
+
+# arithmetic operators
+@binaryinfix
+class Add:
+    OP_STR = '+'
+    IDENT = 0
+    OP_FUNC = op.add
+
+    def simplify(self) -> Function:
+        ...
